c655a19edccc49c2ddc1eafefcf99cf6146d25d6
신지섭 <ddslk75@naver.com>
2024-02-24 02:40:13 +0900

    Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 import './App.css';
+
+const NavBtn = styled.button`
+  border: none;
+  background: transparent;
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>
+          주랩추
+        </h2>
+        <div className='user-container'>
+          <span className='login-btn'>로그인</span>
+          <span className='signup-btn'>회원가입</span>
+        </div>
       </header>
+
+      <nav>
+        <div className='sidebar-container'>
+          <NavBtn className='closeSide-btn' onClick={() => {
+            const sidebar = document.querySelector('.sidebar-container');
+            const openBtn = document.querySelector('.openSide-btn');
+            if(sidebar instanceof HTMLDivElement && openBtn instanceof HTMLButtonElement){
+              sidebar.style.left = '-20rem';
+              openBtn.style.visibility = 'visible';
+            }
+          }}>
+            <FontAwesomeIcon icon={faAnglesLeft} />
+          </NavBtn>
+          <ul className='side-content'>
+            <li>해외축구</li>
+            <li>국내축구</li>
+            <li>대표팀</li>
+            <li>해외 클럽팀</li>
+            <li>국내 클럽팀</li>
+          </ul>
+        </div>
+        <NavBtn className='openSide-btn' onClick={() => {
+          const sidebar = document.querySelector('.sidebar-container');
+          const openBtn = document.querySelector('.openSide-btn');
+          if(sidebar instanceof HTMLDivElement && openBtn instanceof HTMLButtonElement){
+            sidebar.style.left = '0';
+            openBtn.style.visibility = 'hidden';
+          } 
+        }}>
+          <FontAwesomeIcon icon={faAnglesRight} />
+        </NavBtn>
+      </nav>
+
+      <div className='left-ad'>
+        AD
+      </div>
+      <div className='right-ad'>
+        AD
+      </div>
+
+      <main>
+        Main
+      </main>
+
     </div>
   );
 }
